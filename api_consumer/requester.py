3187db8800cc59282f3bfb052f43353776ebc3a3
amir <amirkhakshour@gmail.com>
2020-09-17 10:04:19 +0200

    TDD: regression: moved url generation inside APIRequester

--- api_consumer/requester.py
+++ api_consumer/requester.py
@@ -1,16 +1,26 @@
 import requests
 import json
+from api_consumer import settings
 from api_consumer.clients import RequestsRequesterClient
 
 
 class APIRequester:
     client = RequestsRequesterClient
 
-    def __init__(self):
-        self._client = self.client()
+    def __init__(self, api_base=None, client=None):
+        self.api_base = api_base or settings.API_BASE_URL
+        self._client = client or self.client()
 
     def request(self, method, url, params=None):
-        content, status_code, headers = self._client.request(method, url)
+        """
+
+        :param method: HTTP Methods
+        :param url: relative URL to API base url
+        :param params: additional params for API request
+        :return:
+        """
+        abs_url = "%s%s" % (self.api_base, url)
+        content, status_code, headers = self._client.request(method, abs_url)
         return self.interpret_response(content, status_code, headers)
 
     def interpret_response(self, content, status_code, headers):
