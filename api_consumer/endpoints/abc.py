c16fcd7bc82eec88952edd46492195411813fa94
amir <amirkhakshour@gmail.com>
2020-09-17 21:42:29 +0200

    TDD: GREEN: added retrieve functionality to APIEndpoint

--- api_consumer/endpoints/abc.py
+++ api_consumer/endpoints/abc.py
@@ -1,6 +1,5 @@
-import json
 from api_consumer.requester import APIRequester
-from api_consumer import settings
+from urllib.parse import quote_plus
 
 
 class APIEndpoint:
@@ -17,7 +16,19 @@ class APIEndpoint:
         return "%s" % (base,)
 
     @classmethod
+    def instance_url(cls, _id):
+        base = cls.obj_name.replace(".", "/")
+        _id_encoded = quote_plus(_id)
+        return "%s/%s" % (base, _id_encoded)
+
+    @classmethod
     def list(cls, **params):
         url = cls.type_url()
         requester = cls.api_requester()
+        return requester.request("get", url, params)
+
+    @classmethod
+    def retrieve(cls, _id, **params):
+        requester = cls.api_requester()
+        url = cls.instance_url(_id)
         return requester.request("get", url, params)
