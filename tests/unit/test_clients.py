60b526db30f33ff95570ec89792c0c1b75faade7
amir <amirkhakshour@gmail.com>
2020-09-18 14:09:21 +0200

    TDD: RED: handle request exceptions should fail

--- tests/unit/test_clients.py
+++ tests/unit/test_clients.py
@@ -1,4 +1,8 @@
 import pytest
+import api_consumer
+
+
+DUMMY_URL = 'https://dummy'
 
 
 class TestRequesterClientBase:
@@ -18,7 +22,7 @@ class TestRequesterClientBase:
 
     @pytest.fixture
     def request_mock(self, request_mocks):
-        return request_mocks[self.REQUESTER_CLIENT.name]
+        return request_mocks[str(self.REQUESTER_CLIENT.name)]
 
     def make_request(self, method, url, headers):
         client = self.REQUESTER_CLIENT()
@@ -43,3 +47,29 @@ class TestRequesterClientBase:
         return mock_error
 
 
+class TestRequestsRequesterClient(TestRequesterClientBase):
+    REQUESTER_CLIENT = api_consumer.clients.RequestsRequesterClient
+
+    @pytest.fixture
+    def requests_session(self, mocker, request_mocks):
+        return mocker.MagicMock()
+
+    @pytest.fixture
+    def mock_error(self, mocker, requests_session):
+        def mock_error(mock):
+            # make isinstance for error handler work
+            mock.exceptions.Timeout = Exception
+            requests_session.request.side_effect = mock.exceptions.Timeout()
+            # override Session of mocked requests in order to be able to trigger side_effect
+            mock.Session = mocker.MagicMock(return_value=requests_session)
+
+        return mock_error
+
+    def make_request(self, method, url, headers):
+        client = self.REQUESTER_CLIENT()
+        return client.request(method, url, headers)
+
+    def test_exception(self, request_mock, mock_error):
+        mock_error(request_mock)
+        with pytest.raises(api_consumer.exceptions.APIConnectionError):
+            self.make_request("get", DUMMY_URL, {})
